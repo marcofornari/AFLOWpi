8f4f92758a6e7e22f232f1efca2439adf88331cf
Andrew Supka <supka1ar@cmich.edu>
2017-05-11 16:01:06 -0400

    tidied up perovskite phonon example

--- examples/Beginner/Phonon_Perovskite/phonon.py
+++ examples/Beginner/Phonon_Perovskite/phonon.py
@@ -1,7 +1,7 @@
 import AFLOWpi
 
 # Create AFLOWpi session
-session = AFLOWpi.prep.init('Phonon', 'Perovskite',
+session = AFLOWpi.prep.init('Phonon', 'Perovskite_3',
                             config='./phonon.config')
 # Generate a calculation set from a reference input file
 allvars={"_AFLOWPI_A_":("Ba","Pb",),
@@ -21,7 +21,7 @@ calcs.change_input("&electrons","conv_thr","1.0D-16")
 #do another relax just to be safe
 calcs.vcrelax()
 #do phonon calculations with 2x2x2 supercell
-calcs.phonon(mult_jobs=True,nrx1=3,nrx2=3,nrx3=3,innx=3,
+calcs.phonon(mult_jobs=True,nrx1=2,nrx2=2,nrx3=2,innx=3,
              de=0.003,LOTO=True,field_strength=0.003,
              disp_sym=True,atom_sym=False,)
 #plot the phonons
