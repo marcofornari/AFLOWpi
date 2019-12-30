17773d98c4589d608c935ec9ab1b00359a32a24d
Andrew Supka <supka1ar@cmich.edu>
2019-12-30 00:00:46 -0500

    addding a few things i missed

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+#__all__ = ['scfuj','prep','run','plot','retr','pseudo','db',]
+import AFLOWpi.prep
+import AFLOWpi.run
+import AFLOWpi.plot
+import AFLOWpi.retr
+import AFLOWpi.pseudo
+import AFLOWpi.db
+import AFLOWpi.scfuj
+import AFLOWpi.aflowlib
+import AFLOWpi.elph
+import AFLOWpi.environ
