92a1a4122f918aa63b72b1deb3ddf7185ee1fe2e
Andrew Supka <supka1ar@cmich.edu>
2020-07-17 09:39:54 -0400

    updated mHT example

--- examples/Beginner/mHT/mHT.py
+++ examples/Beginner/mHT/mHT.py
@@ -6,17 +6,17 @@ session = AFLOWpi.prep.init('mHT', 'zincblende',
 # choose the values for the keywords in the ref file
 allvars={}
 allvars.update(
-_AFLOWPI_A_ = ('Al',),
-_AFLOWPI_B_ = ('As',),)
+    _AFLOWPI_A_ = ('Al','In'),
+    _AFLOWPI_B_ = ('As','P'),)
 # form the calculation set from ref input and allvars dict
-calcs = session.from_file(['mHT.in'])
+calcs = session.scfs(allvars,'mHT.ref')
 # relax the structure
 calcs.vcrelax()
 #updated cell parameters
 # calculate the the DOS and PDOS for each 
 calcs.vcrelax()
 calcs.vcrelax()
-calcs.dos()
+calcs.dos(n_conduction=2.0,kp_factor=2.0)
 calcs.plot.opdos()
 # calculate the bands for each
 calcs.bands()
@@ -24,7 +24,7 @@ calcs.bands()
 # and atom projected DOS for each
 calcs.plot.bands(DOSPlot='APDOS')
 # run the calculation workflow
-#calcs.submit()
+calcs.submit()
 
 
 
