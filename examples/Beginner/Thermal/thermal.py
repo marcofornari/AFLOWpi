9791e33c87a97507423367564644d36171c6068f
Andrew Supka <supka1ar@cmich.edu>
2020-07-17 13:01:56 -0400

    added DC  thermal example

--- examples/Beginner/Thermal/thermal.py
+++ examples/Beginner/Thermal/thermal.py
@@ -1,6 +1,7 @@
 import AFLOWpi
 # start the AFLOWpirame session
-session = AFLOWpi.prep.init('AFLOWpi_tests', 'thermal_ZB',config='./thermal.config')
+session = AFLOWpi.prep.init('AFLOWpi_tests', 
+                            'thermal_ZB',config='./thermal.config')
 # choose the values for the keywords in the ref file
 allvars={}
 allvars.update(
@@ -10,11 +11,23 @@ _AFLOWPI_B_ = ('P',),)
 calcs = session.scfs(allvars,'thermal.ref',)
 # relax the structure
 calcs.vcrelax()
+# change the thresholds in the input files
+# changing input for a step are called after
+# the call for the step. the change_calcs
+# below affect the vc-relax above
+calcs.change_input("&control","etot_conv_thr","1.0D-5")
+calcs.change_input("&control","forc_conv_thr","1.0D-4")
+calcs.change_input("&electrons","conv_thr","1.0D-12")
 calcs.vcrelax()
-calcs.relax()
-calcs.scf()
-# do thermal calc with forward difference finite difference calc of gruneisen parameter
-calcs.thermal(delta_volume=0.04,mult_jobs=True,nrx1=2,nrx2=2,nrx3=2,innx=2,de=0.01,LOTO=False,field_strength=0.002,disp_sym=True,atom_sym=False,central_diff=True)
+
+calcs.phonon(mult_jobs=True,nrx1=2,nrx2=2,nrx3=2,innx=2,
+             LOTO=False,disp_sym=True,atom_sym=True,)
+
+# do thermal calc with forward difference finite 
+# difference calc of gruneisen parameter
+calcs.thermal(delta_volume=0.04,mult_jobs=True,nrx1=2,
+              nrx2=2,nrx3=2,innx=2,de=0.01,LOTO=False,
+              disp_sym=True,atom_sym=True,central_diff=True)
 
 # FYI: this won't have LOTO splitting
 calcs.plot.phonon(postfix='inCM',THz=False,runlocal=False)
@@ -23,7 +36,6 @@ calcs.plot.thermal_cond(temp_range=[250,800])
 #plot frequency resolved (projected part still needs work)
 calcs.plot.gruneisen()
 
-calcs.elastic()
 calcs.submit()
 
 
