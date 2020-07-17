3df92603070892ba8df42ea72ea8e542a94100a3
Andrew Supka <supka1ar@cmich.edu>
2020-07-17 10:06:19 -0400

    updated AHC example

--- examples/Beginner/PAOFLOW_SHC_AHC/PAOFLOW_AFLOWPI.py
+++ examples/Beginner/PAOFLOW_SHC_AHC/PAOFLOW_AFLOWPI.py
@@ -6,17 +6,17 @@ session = AFLOWpi.prep.init('AHC_SHC', 'fe',
 # load an already made input file (no AFLOWpi keywords)
 calcs = session.from_file('PAOFLOW_PW.in')
 # relax the structure
-calcs.scf()
+
 # run calcs for spin polarized NiO for optical and
 # transport properties with WanT at 300K and 400K 
 
 tb = calcs.tight_binding()
 
 tb.dos()
-tb.bands(band_topology=True,fermi_surface=True)
+tb.bands(band_topology=True)
 
 #if t_tensor is omitted (default), all components are calculated
-tb.transport(t_tensor=None)
+tb.transport()
 
 # components for epsilon
 d_tensor = [[0,1], # xy component
